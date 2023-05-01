81fb96c43e1d0969c45e156a2e8a51d438f442b5
Shafiu Ibn Yushawu <yishafiu@gmail.com>
2023-05-01 21:45:17 +0000

    query and update animals table

--- queries.sql
+++ queries.sql
@@ -1,18 +1,52 @@
 /*Queries that provide answers to the questions from all projects.*/
 
 SELECT * from animals WHERE name LIKE '%mon';
-
 SELECT name from animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-01-01';
-
 SELECT name from animals WHERE neutered = true AND escape_attempts < 3;
-
 SELECT date_of_birth from animals WHERE name IN ('Agumon', 'Pikachu');
-
 SELECT name, escape_attempts FROM animals WHERE weight_kg < 10.5;
-
 SELECT name from animals WHERE neutered=true;
-
 SELECT * from animals WHERE name <> 'Gabumon';
-
 SELECT * from animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
 
+BEGIN;
+UPDATE animals SET species = 'unspecified';
+SELECT * FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
+
+BEGIN;
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+UPDATE animals SET species = 'pokemon' WHERE species = 'unspecified';
+COMMIT;
+SELECT * FROM animals;
+
+BEGIN;
+DELETE FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
+
+BEGIN;
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+SAVEPOINT before_weight_update;
+UPDATE animals SET weight_kg = weight_kg * -1;
+ROLLBACK TO before_weight_update;
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+COMMIT;
+SELECT * FROM animals;
+
+
+SELECT COUNT(*) FROM animals;
+
+SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
+
+SELECT AVG(weight_kg) FROM animals;
+
+SELECT species, MIN(weight_kg), MAX(weight_kg)
+FROM animals
+GROUP BY species;
+
+SELECT species, AVG(escape_attempts) AS avg_escape_attempts
+FROM animals
+WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31'
+GROUP BY species;
