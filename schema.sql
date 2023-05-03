33173151cd2e72714cb009b0d70c34a393dc03cf
Shafiu Ibn Yushawu <yishafiu@gmail.com>
2023-05-03 06:58:42 +0000

    modified animals table id to auto increase

--- schema.sql
+++ schema.sql
@@ -1,5 +1,6 @@
 /* Database schema to keep the structure of entire database. */
 
+-- create animal table 
 CREATE TABLE animals (
     id int primary key not null,
     name varchar(100) not null,
@@ -10,3 +11,21 @@ CREATE TABLE animals (
     species varchar(100)
 );
 
+-- owners table 
+create table owners (
+    id int generated always as identity,
+    full_name varchar(255) not null,
+    age int not null
+);
+
+-- species table 
+create table species (
+    id int generated always as identity,
+    name varchar(255) not null
+);
+
+-- modify the animal table
+alter table animals 
+    drop constraint if exists animals_pkey;
+alter table animals 
+    alter column id add generated always as identity primary key;
