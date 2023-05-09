400e36e9d193bb0ab4e425062d2d2463a8919947
edubew <edubew98@gmail.com>
2023-05-09 16:26:53 +0300

    Create tables from schema diagram

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -21,3 +21,27 @@ create table invoices (
     constraint fk_invoices_medical_histories foreign key(medical_history_id) references medical_histories(id)
 );
 
+create table invoice_items (
+    id int primary key,
+    unit_price decimal(5, 2),
+    quantity int,
+    total_price decimal(5,2),
+    invoice_id int,
+    treatment_id int,
+    constraint fk_invoice_item_invoices foreign key(invoice_id) references invoices(id),
+    constraint fk_invoice_item_treatment foreign key(treatment_id) references treatments(id)
+);
+
+create table treatments (
+    id int primary key,
+    type varchar(100),
+    name varchar(255)
+);
+
+create table medical_histories_treatment (
+    id int primary key,
+    medical_history_id int,
+    treatment_id int,
+    constraint fk_medical_histories foreign key(medical_history_id) references medical_histories(id),
+    constraint fk_treatments foreign key(treatment_id) references treatments(id)
+);
